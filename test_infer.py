288b74315665b8b6b1e800b21e1cebea24e226b6
Soenke Bartels <soenke@pop-os.localdomain>
2020-12-17 19:40:45 +0100

    clean up

--- test_infer.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from wfl_opt_data import *
-
-nodeoi = 23
-plt.scatter(grid[nodeoi][0], grid[nodeoi][1])
-plt.imshow(infer_matrix[nodeoi], cmap='hot', interpolation='bicubic')
-plt.show()
